9639fbcbd8b154b3b925996599843bf7237c8e42
Chelsea Clay Work <chelseaclayyy@yahoo.com>
2016-09-16 11:19:44 -0400

    fixed js
    made mobile menu disappear after section link is clicked.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
 $("#menu").click(function() {
   var slideClass = $(".slideMenu").hasClass("slide");
-
+  $(".slideMenu").show();
   if (slideClass === true) {
     $(".slideMenu").removeClass("slide");
   } else if (slideClass === false) {
     $(".slideMenu").addClass("slide");
   }
+});
+
+// $(".nav-bar").click(function() {
+//   $(".slideMenu").hide();
+// });
+
+
+$(".nav-bar").click(function() {
+  $(".slideMenu").removeClass("slide");
+  $(".slideMenu").hide();
 });
